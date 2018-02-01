be5af32440654d63134d48e1c851f9968c76a0a1
Артём Павленко <lytr777@MacBook-Pro-Artem.local>
2018-02-01 18:48:55 +0300

    add start script

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+touch out/new_out
+python main.py > out/new_out
--- untar.sh
+++ untar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lingeling_path="t_lingeling"
+lingeling_path="lingeling"
 mkdir $lingeling_path
 tar -xzf lingeling.tar -C $lingeling_path --strip-components 1
 cd $lingeling_path
