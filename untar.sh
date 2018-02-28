3e46417f88eef4e02ce503f07cceebc426cd7df6
Артём Павленко <>
2018-02-28 21:58:39 +0300

    add minisat and try to fix rokk

--- untar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lingeling_path="lingeling"
-mkdir $lingeling_path
-tar -xf lingeling.tar -C $lingeling_path --strip-components 1
-cd $lingeling_path
-./configure.sh && make
--- untar_rokk.sh
+++ untar_rokk.sh
@@ -3,5 +3,18 @@
 rokk_path="rokk"
 mkdir $rokk_path
 tar -xf rokk.tar -C $rokk_path --strip-components 1
-cd $rokk_path
+
+cd $rokk_path/code/sources
+cp rokk/core/Makefile core_Make
+cp rokk/simp/Makefile simp_Make
+rm -rf rokk
+
+mkdir $rokk_path
+tar -xf ../../../minisat.tar -C $rokk_path --strip-components 1
+rm -f rokk/core/Makefile
+rm -f rokk/simp/Makefile
+cp core_Make rokk/core/Makefile
+cp simp_Make rokk/simp/Makefile
+
+cd ../..
 ./build.sh
