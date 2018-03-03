6cbba8ffa07ab47e2f9f18da281737c9c7cd7e20
root <lytr777@mail.ru>
2018-03-03 19:08:38 +0300

    fix rokk

--- untar_rokk.sh
+++ untar_rokk.sh
@@ -3,18 +3,6 @@
 name="rokk"
 mkdir $name
 tar -xf tars/rokk.tar -C $name --strip-components 1
+cd $name
 
-cd $name/code/sources
-cp rokk/core/Makefile core_Make
-cp rokk/simp/Makefile simp_Make
-rm -rf rokk
-
-mkdir $name
-tar -xf ../../../tars/minisat.tar -C $name --strip-components 1
-rm -f rokk/core/Makefile
-rm -f rokk/simp/Makefile
-cp core_Make rokk/core/Makefile
-cp simp_Make rokk/simp/Makefile
-
-cd ../..
 ./build.sh
