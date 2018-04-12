b1fece9c391c7b0379e2c39e6efffdc053416826
Артём Павленко <>
2018-04-12 17:41:36 +0300

    change rokk utils

--- tools/start_rokk.py
+++ tools/start_rokk.py
@@ -1,89 +1,66 @@
 import sys
-import os
 import subprocess
-import tempfile
-import random
 
 
-def parse_out(code, out):
-    t = 0
-    st = None
+def parse_out(out):
+    i = 0
     lines = out.split('\n')
-    for line in lines:
-        if line.startswith("c CPU time"):
-            t_str = ""
-            for c in line.split(':')[1]:
-                if c.isdigit() or c == '.':
-                    t_str += c
-            t = float(t_str)
+    while not lines[i].startswith("i CPU time"):
+        i += 1
+    t = float(lines[i].split(':')[1])
 
-        if line.startswith("s "):
-            st = line.split(' ')[1]
+    while not lines[i].startswith("i "):
+        i += 1
+    st = lines[i].split(' ')[1]
 
-    return t, st
+    s = ''
+    c = ''
+    i += 1
+    if st == 'SATISFIABLE':
+        s = lines[i]
+    elif st == 'PRESATELITED':
+        while i < len(lines):
+            c += lines[i] + '\n'
+            i += 1
+
+    return t, st, s, c
 
 
 if len(sys.argv) < 2:
-    print "USAGE: rokk_py.py <input CNF> [time limit]"
+    print "USAGE: rokk_py.py [time limit]"
     exit(1)
 
-cnf = sys.argv[1]
 time_limit = None
-if len(sys.argv) > 2:
-    time_limit = int(sys.argv[2])
+if len(sys.argv) > 1:
+    time_limit = int(sys.argv[1])
+
+cnf = ''
+for l in sys.stdin:
+    cnf += l
 
 elite_path = './rokk/binary/SatELite_release'
 solver_path = './rokk/binary/rokk_static'
-number = str(int(random.random() * 1000000))
 
-files = []
-for ex in ['.cnf', '.vmap', '.elim']:
-    files.append(tempfile.NamedTemporaryFile(prefix=number, suffix=ex).name)
+p = subprocess.Popen([elite_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = p.communicate(cnf)[0]
 
-l_args = [elite_path, cnf, files[0], files[1], files[2]]
-p = subprocess.Popen(l_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output = p.communicate()[0]
-code = p.poll()
+elite_time, status, solution, cnf = parse_out(output)
 
-elite_time, status = parse_out(code, output)
-time = 0
-solution = ''
-
-if status is None:
-    p_flag = False
-    files.append(tempfile.NamedTemporaryFile(prefix=number, suffix='.result').name)
+if status == 'SATISFIABLE' or status == 'UNSATISFIABLE':
+    print str(elite_time) + ' p'
+    print status
+    print solution
+elif cnf != '':
     l_args = [solver_path]
     if time_limit is not None:
         l_args.append('-cpu-lim=' + str(time_limit))
-    l_args.append(files[0])
-    l_args.append(files[3])
-
     p = subprocess.Popen(l_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = p.communicate()[0]
-    code = p.poll()
 
-    time, status = parse_out(code, output)
+    output = p.communicate(cnf)[0]
+    time, status, solution, cnf = parse_out(output)
 
-    if (status is not None) and (status == 'SATISFIABLE'):
-        l_args = [elite_path, '+ext', cnf, files[3], files[1], files[2]]
-        p = subprocess.Popen(l_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = p.communicate()[0]
-
-        solution = output.split("\n")[1][2:]
-else:
-    p_flag = True
-
-with open(files[0]) as f:
-    lines = f.readlines()
-    if lines[0].__contains__('#vars: 0'):
-        p_flag = True
-
-print str(elite_time + time) + (' p' if p_flag else '')
-print status
-print solution
-
-for ff in files:
-    if os.path.isfile(ff):
-        os.remove(ff)
+    print str(elite_time + time)
+    print status
+    print solution
 
 exit(0)
--- untar_rokk.sh
+++ untar_rokk.sh
@@ -5,6 +5,15 @@ mkdir $name
 tar -xf tars/rokk.tar -C $name --strip-components 1
 cd $name
 
+rm -f code/sources/rokk/core/Main.cc
+cp ../tools/rokk_re/core/Main.cc code/sources/rokk/core/Main.cc
+
+rm -f code/sources/SatELite/SatELite/Main.C
+cp ../tools/SatElite/Main.C code/sources/SatELite/SatELite/Main.C
+
+rm -f code/sources/SatELite/SatELite/Solver_subsume.C
+cp ../tools/SatElite/Solver_subsume.C code/sources/SatELite/SatELite/Solver_subsume.C
+
 chmod +x build.sh
 ./build.sh
 
