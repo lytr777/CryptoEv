6b8763fd5d4dbe8b823f693e7cf4c390cd819603
Артём Павленко <>
2018-04-30 03:58:03 +0300

    new test

--- perf_test.sh
+++ perf_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=4 --mem=10G -t 6000 --mail-type=END --mail-user=lytr777@mail.ru main.sh p1 configurations/perf_test.json
+sbatch --cpus-per-task=4 --mem=10G -t 6000 --mail-type=END --mail-user=lytr777@mail.ru main.sh p2 configurations/perf_test.json
