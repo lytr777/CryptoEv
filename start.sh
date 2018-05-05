08881226945bf5e2b08729f982cc50c4458f499b
Артём Павленко <>
2018-05-05 13:32:11 +0300

    true case fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=16 --mem=10G -t 6000 --mail-type=END --mail-user=lytr777@mail.ru f_true.sh
+sbatch --cpus-per-task=16 --mem=40G -t 6000 --mail-type=END --mail-user=lytr777@mail.ru f_true.sh
