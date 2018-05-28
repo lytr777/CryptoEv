80575b3d5d61627b5743dce96adb1cb5120f61ba
Артём Павленко <>
2018-05-28 04:39:56 +0300

    case ad_f

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ev1000
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ad_t
