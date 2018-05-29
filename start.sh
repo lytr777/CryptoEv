8fd025e44e1594f25e84b63e2ee2a341a08f64a2
Артём Павленко <>
2018-05-29 16:22:43 +0300

    adaptive test

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11_1 configurations/st_ex/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12_1 configurations/st_ex/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15_1 configurations/st_ex/15.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11a_1 configurations/ad_ex/11.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12a_1 configurations/ad_ex/12.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15a_1 configurations/ad_ex/15.json
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11_2 configurations/st_ex/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12_2 configurations/st_ex/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15_2 configurations/st_ex/15.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11a_2 configurations/ad_ex/11.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12a_2 configurations/ad_ex/12.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15a_2 configurations/ad_ex/15.json
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11_3 configurations/st_ex/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12_3 configurations/st_ex/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15_3 configurations/st_ex/15.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11a_3 configurations/ad_ex/11.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12a_3 configurations/ad_ex/12.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15a_3 configurations/ad_ex/15.json
