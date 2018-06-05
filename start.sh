164145033929bf401ff224539f18968b4c28d16d
Артём Павленко <>
2018-06-05 04:39:19 +0300

    new case

--- start.sh
+++ start.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_a_1 configurations/ad_ex/gen.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_a_2 configurations/ad_ex/gen.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_a_3 configurations/ad_ex/gen.json
-
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11ao_1 configurations/ad_ex_out/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12ao_1 configurations/ad_ex_out/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15ao_1 configurations/ad_ex_out/15.json
-
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11ao_2 configurations/ad_ex_out/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12ao_2 configurations/ad_ex_out/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15ao_2 configurations/ad_ex_out/15.json
-
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 11ao_3 configurations/ad_ex_out/11.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 12ao_3 configurations/ad_ex_out/12.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh 15ao_3 configurations/ad_ex_out/15.json
-
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_ao_1 configurations/ad_ex_out/gen.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_ao_2 configurations/ad_ex_out/gen.json
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh gen_ao_3 configurations/ad_ex_out/gen.json
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ap_tr_1
