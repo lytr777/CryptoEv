6963387d13909c21c989c0bc80645540f9db9309
Артём Павленко <>
2018-06-07 13:20:44 +0300

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ap_biv_1
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ap_biv_2
-sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ap_biv_2
+sbatch --cpus-per-task=32 --mem=30G -t 10000 --mail-type=END --mail-user=lytr777@mail.ru main.sh ap_biv_3
