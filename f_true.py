b804a08a24ce3d506910f6113b9b921501723d38
Артём Павленко <>
2018-08-08 17:48:53 +0300

    fix naming

--- f_true.py
+++ f_true.py
@@ -16,16 +16,16 @@ parser.add_argument('-v', metavar='0', type=int, default=0, help='[0-3] verbosit
 parser.add_argument('-d', metavar='path', type=str, help='path to debug file')
 
 args = parser.parse_args()
-_, meta_p, mf_p = configurator.load('true', {})
-m_function = meta_p["predictive_function"]
+_, meta_p, pf_p = configurator.load('true', {})
+p_function = meta_p["predictive_function"]
 
 true_log_file = constant.true_log_path + args.id
 
-mf_p["debugger"] = Debugger(args.d, args.v)
+pf_p["debugger"] = Debugger(args.d, args.v)
 if args.d is not None:
     open(args.d, 'w+').close()
 
-algorithm = mf_p["key_generator"]
+algorithm = pf_p["key_generator"]
 cnf_path = constant.cnfs[algorithm.tag]
 cnf = CnfParser().parse_for_path(cnf_path)
 
@@ -35,20 +35,20 @@ rs = np.random.RandomState()
 cg = CaseGenerator(algorithm, cnf, rs, backdoor)
 
 
-mf_p["solver_wrapper"].check_installation()
+pf_p["solver_wrapper"].check_installation()
 with open(true_log_file, 'w+') as f:
     f.write("-- key generator: %s\n" % algorithm('').tag)
-    f.write("-- solver: %s\n" % mf_p["solver_wrapper"].info["tag"])
-    f.write("-- pf type: %s\n" % m_function.type)
-    if m_function.type == "ibs":
-        f.write("-- time limit: %s\n" % mf_p["time_limit"])
-    f.write("-- samples size: %d\n" % mf_p["N"])
+    f.write("-- solver: %s\n" % pf_p["solver_wrapper"].info["tag"])
+    f.write("-- pf type: %s\n" % p_function.type)
+    if p_function.type == "ibs":
+        f.write("-- time limit: %s\n" % pf_p["time_limit"])
+    f.write("-- samples size: %d\n" % pf_p["N"])
     f.write("-- backdoor: %s\n" % backdoor)
     f.write("------------------------------------------------------\n")
 
-mf = m_function(mf_p)
+mf = p_function(pf_p)
 result = mf.compute(cg)
-value, mf_log = result[0], result[1]
+value, pf_log = result[0], result[1]
 
 with open(true_log_file, 'a') as f:
-    f.write(mf_log + "true value: %.7g\n" % value)
+    f.write(pf_log + "true value: %.7g\n" % value)
--- mpi_f_true.py
+++ mpi_f_true.py
@@ -18,12 +18,12 @@ parser.add_argument('-v', metavar='0', type=int, default=0, help='[0-3] verbosit
 parser.add_argument('-d', metavar='path', type=str, help='path to debug file')
 
 args = parser.parse_args()
-_, meta_p, mf_p = configurator.load('true', {})
-m_function = meta_p["predictive_function"]
+_, meta_p, pf_p = configurator.load('true', {})
+p_function = meta_p["predictive_function"]
 
 true_log_file = constant.true_log_path + args.id
 
-algorithm = mf_p["key_generator"]
+algorithm = pf_p["key_generator"]
 cnf_path = constant.cnfs[algorithm.tag]
 cnf = CnfParser().parse_for_path(cnf_path)
 
@@ -37,49 +37,49 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if args.d is None:
-    mf_p["debugger"] = Debugger(verb=args.v)
+    pf_p["debugger"] = Debugger(verb=args.v)
 else:
     rank_debug_file = "%s_%d" % (args.d, rank)
-    mf_p["debugger"] = Debugger(rank_debug_file, args.v)
+    pf_p["debugger"] = Debugger(rank_debug_file, args.v)
     open(rank_debug_file, 'w+').close()
 
-mf_p["solver_wrapper"].check_installation()
+pf_p["solver_wrapper"].check_installation()
 if rank == 0:
     with open(true_log_file, 'w+') as f:
         f.write("-- key generator: %s\n" % algorithm('').tag)
-        f.write("-- solver: %s\n" % mf_p["solver_wrapper"].info["tag"])
-        f.write("-- pf type: %s\n" % m_function.type)
-        if m_function.type == "ibs":
-            f.write("-- time limit: %s\n" % mf_p["time_limit"])
-        f.write("-- samples size: %d\n" % mf_p["N"])
+        f.write("-- solver: %s\n" % pf_p["solver_wrapper"].info["tag"])
+        f.write("-- pf type: %s\n" % p_function.type)
+        if p_function.type == "ibs":
+            f.write("-- time limit: %s\n" % pf_p["time_limit"])
+        f.write("-- samples size: %d\n" % pf_p["N"])
         f.write("-- backdoor: %s\n" % backdoor)
         f.write("------------------------------------------------------\n")
 
-mf_p["mpi_call"] = True
+pf_p["mpi_call"] = True
 
-(quotient, remainder) = divmod(mf_p["N"], size)
+(quotient, remainder) = divmod(pf_p["N"], size)
 rank_N = quotient + (1 if remainder > 0 else 0)
 real_N = rank_N * size
 
-mf_p["N"] = rank_N
+pf_p["N"] = rank_N
 
 if rank == 0:
     start_work_time = now()
 
-    mf = m_function(mf_p)
-    result = mf.compute(cg)
+    pf = p_function(pf_p)
+    result = pf.compute(cg)
 
     cases = comm.gather(result[2], root=0)
     cases = np.concatenate(cases)
 
     time = now() - start_work_time
-    final_result = mf.handle_cases(cg, cases, time)
-    value, mf_log = final_result[0], final_result[1]
+    final_result = pf.handle_cases(cg, cases, time)
+    value, pf_log = final_result[0], final_result[1]
 
     with open(true_log_file, 'a') as f:
-        f.write(mf_log + "true value: %.7g\n" % value)
+        f.write(pf_log + "true value: %.7g\n" % value)
 else:
-    mf = m_function(mf_p)
-    result = mf.compute(cg)
+    pf = p_function(pf_p)
+    result = pf.compute(cg)
 
     comm.gather(result[2], root=0)
